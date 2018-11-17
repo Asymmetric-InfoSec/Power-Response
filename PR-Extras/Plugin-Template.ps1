140d15edd317dda95ef456ffcda4ca7c81600e0f
5ynax <5ynax@5ynax.com>
2018-11-17 08:17:27 -0600

    Added Plugin template to extras folder

---
+++ PR-Extras/Plugin-Template.ps1
@@ -0,0 +1,21 @@
+<#
+
+.SYNOPSIS
+    Plugin-Name:
+    
+.Description
+
+
+.NOTES
+
+
+.EXAMPLE
+    Author:
+    Date Created:
+    Twitter: 
+    
+    Last Modified By:
+    Last Modified Date:
+    Twitter:
+
+#>
