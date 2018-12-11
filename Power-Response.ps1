599bbda146fae42cbb2b1c058f39e1ddeac4c0ad
5ynax <5ynax@5ynax.com>
2018-12-11 15:27:43 -0600

    added small change to prevent issue with menu

--- Power-Response.ps1
+++ Power-Response.ps1
@@ -425,7 +425,7 @@ function Invoke-ShowCommand {
         # Initialize empty $Param(eter) return HashTable
         $Param = @{}
             #For plugins with no parameters, write-host to run plugin
-        if ($CommandParameters.Count -eq 0) {
+        if ($CommandParameters.Count -eq 0 -and !$script:Location.PSIsContainer) {
             Write-Host "`r`nNo parameters detected. Type run to execute plugin."
         }
         elseif ($CommandParameters.Count -gt 0) {
