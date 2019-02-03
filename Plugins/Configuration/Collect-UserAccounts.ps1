cd50b572ce6c5f396b3b63b80718fd0be5ba340c
valrkey <10157559+valrkey@users.noreply.github.com>
2019-02-03 05:49:37 +0000

    Added net localgroup administrators

--- Plugins/Configuration/Collect-UserAccounts.ps1
+++ Plugins/Configuration/Collect-UserAccounts.ps1
@@ -38,14 +38,20 @@ process {
     # Collect the local and network user data from the remote systems
     $Local = Invoke-Command -ComputerName $ComputerName -ScriptBlock { Get-CIMInstance -Class "Win32_UserAccount" }
     $Network = Invoke-Command -ComputerName $ComputerName -ScriptBlock { Get-CIMInstance -Class 'Win32_NetworkLoginProfile' }
+    $RawAdmins = Invoke-Command -ComputerName $ComputerName -ScriptBlock { net localgroup administrators }
+
+    # Jankcity way to parse administrators
+    $Admins = $rawadmins | select -skip 6 | Where-Object { $PSItem -and $PSItem -NotMatch 'The command completed successfully.' } | Foreach-Object { [PSCustomObject]@{ Account=$PSItem } }
 
     # If we are providing PowerResponse output, explicitly send data to separate files
     if (Get-Command -Name 'Out-PRFile' -ErrorAction 'SilentlyContinue') {
         $Local | Select-Object -Property 'Name','Caption','Disabled','AccountType','SID','Domain','Description' | Out-PRFile -Append 'Local'
         $Network | Select-Object -Property 'Name','UserID','Caption','Profile','LastLogon','NumberOfLogons','BadPasswordCount' | Out-PRFile -Append 'Network'
+        $Admins | Out-PRFile -Append 'Admin'
     } else {
         # Else, return the objects to the console
         $Local
         $Network
+        $Admins
     }
 }
