f1c407e2ff104986a912e163857e3a50fbf774c2
5ynax <5ynax@asymmetricinfosec.com>
2019-02-08 20:09:11 -0600

    Scheduled tasks plugins and prefetch plugin fix

---
+++ Plugins/Configuration/Collect-ScheduledTasks.ps1
@@ -0,0 +1,82 @@
+<#
+
+.SYNOPSIS
+    Plugin-Name: Collect-ScheduledTasks.ps1
+    
+.Description
+    This plugin is used to collect scheduled task configuration files from
+    a remote host. By default, the plugin will recursively copy all files from
+    C:\Windows\System32\Tasks, however, the plugin can also perform targeted
+    extraction by specifying a scheduled task name using the -TaskName parameter
+
+.EXAMPLE
+
+    Stand Alone Execution
+
+    .\Collect-ScheduledTasks.ps1 -ComputerName Test-PC
+
+    Power-Response Execution
+
+    Set ComputerName Test-PC
+    Run
+
+    OR
+
+    Set ComputerName Test-PC
+    Set TaskName TestTask
+    Run
+
+.NOTES
+    Author: Drew Schmitt
+    Date Created: 2/8/2019
+    Twitter: @5ynax
+    
+    Last Modified By:
+    Last Modified Date:
+    Twitter:
+  
+#>
+
+param (
+
+    [Parameter(Mandatory=$true,Position=0)]
+    [string[]]$ComputerName,
+    [Parameter(Mandatory=$false,Position=1)]
+    [string]$TaskName
+
+    )
+
+process{
+
+    # Set $Output for where to store recovered scheduled task files
+    $Output= ("{0}\ScheduledTasks" -f $global:PowerResponse.OutputPath)
+
+    # Create Subdirectory in $global:PowerResponse.OutputPath for storing scheduled tasks
+    If (-not (Test-Path $Output)) {
+        New-Item -Type Directory -Path $Output | Out-Null
+    }   
+
+    foreach ($Computer in $ComputerName) {
+
+        # Create session on remote host
+        $Session = New-PSSession -ComputerName "$Computer"
+
+        if ($TaskName){
+
+             # Copy specified task into $Output
+            Copy-Item "C:\Windows\System32\Tasks\$TaskName" -Destination "$Output\" -FromSession $Session -Force -ErrorAction SilentlyContinue
+
+        } else {
+
+            # Recursively copy directory contents into $Output
+            Copy-Item "C:\Windows\System32\Tasks\" -Recurse -Destination "$Output\" -FromSession $Session -Force -ErrorAction SilentlyContinue
+
+
+        }
+
+        #Close PS Remoting Session
+        $Session | Remove-PSSession
+    
+    }
+
+}
--- Plugins/Process/Collect-Prefetch.ps1
+++ Plugins/Process/Collect-Prefetch.ps1
@@ -1,23 +1,31 @@
 <#
 
 .SYNOPSIS
-    Plugin-Name: Collect-Prefetch
+    Plugin-Name: Collect-Prefetch.ps1
     
 .Description
 
     This plugin will retrieve prefetch files from a remote host and move them to a specified
-    output directory based on the the output path provided from Power-Response. 
+    output directory based on the the output path provided from Power-Response. By default,
+    this plugin will retrieve all prefetch files in the C:\Windows\Prefetch directory, 
+    however, you can specify a specific prefetch file to retrieve using the -PrefetchName
+    parameter.
 
 .EXAMPLE
 
     Stand Alone Execution:
 
-    .\Collect-Prefetch -ComputerName Test-PC
-    Note: Output most likely will end up in C:\Prefetch
+    .\Collect-Prefetch.ps1 -ComputerName Test-PC
 
     Power-Response Execution
 
     Set ComputerName Test-PC
+    Run
+
+    OR
+
+    Set ComputerName Test-PC
+    Set PrefetchName 7ZFM.EXE-3129C294.pf
     Run
 
 .NOTES
@@ -34,7 +42,9 @@
 param (
 
     [Parameter(Mandatory=$true,Position=0)]
-    [string[]]$ComputerName
+    [string[]]$ComputerName,
+    [Parameter(Mandatory=$false,Position=1)]
+    [string[]]$PrefetchName
 
     )
 
@@ -53,8 +63,19 @@ process{
         # Create session on remote host
         $Session = New-PSSession -ComputerName "$Computer"
 
-        Copy-Item "C:\Windows\Prefetch\*" -Destination "$Output\" -FromSession $Session -Force -ErrorAction SilentlyContinue
+        If ($PrefetchName){
 
+            #Copy specified prefetch file to $Output
+            Copy-Item "C:\Windows\Prefetch\$PrefetchName" -Destination "$Output\" -FromSession $Session -Force -ErrorAction SilentlyContinue
+
+        }else{
+
+            # Recursively copy all prefetch files to $Output
+            Copy-Item "C:\Windows\Prefetch\" -Recurse -Destination "$Output\" -FromSession $Session -Force -ErrorAction SilentlyContinue
+
+        }
+
+        #Close PS remoting session
         $Session | Remove-PSSession
     
     }
