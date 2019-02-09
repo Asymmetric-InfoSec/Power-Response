630876fd452c1a2b7d3ca00f89a379116f348a61
5ynax <5ynax@asymmetricinfosec.com>
2019-02-09 10:33:09 -0600

    Collect-WMIObjects.ps1

---
+++ Plugins/Configuration/Collect-WMIObjects.ps1
@@ -0,0 +1,64 @@
+<#
+
+.SYNOPSIS
+    Plugin-Name: Collect-WMIObjects.ps1
+    
+.Description
+    This plugin collects WMI event filters, consumers, and bindigs for a remote 
+    machine.
+
+.EXAMPLE
+
+    Stand Alone Execution
+
+    .\Collect-WMIObjects.ps1 -ComputerName Test-PC
+
+    Power-Response Execution
+
+    Set ComputerName Test-PC
+    Run
+
+.NOTES
+    Author: Drew Schmitt
+    Date Created: 2/9/2019
+    Twitter: @5ynax
+    
+    Last Modified By:
+    Last Modified Date:
+    Twitter:
+  
+#>
+
+param (
+
+    [Parameter(Mandatory=$true,Position=0)]
+    [string[]]$ComputerName
+
+    )
+
+process{
+
+
+    #Loop through machines in $ComputerName to obtain data for each machine (if multiple machines are specified)
+    foreach ($Computer in $ComputerName) {
+
+        # Collect WMI Event Filters
+        $ScriptBlock_Filter = $ExecutionContext.InvokeCommand.NewScriptBlock("Get-WMIObject -Namespace root\Subscription -Class __EventFilter")
+    
+        Invoke-Command -ComputerName $Computer -ScriptBlock $ScriptBlock_Filter
+
+        # Collect WMI Event Consumers
+        $ScriptBlock_Consumer = $ExecutionContext.InvokeCommand.NewScriptBlock("Get-WMIObject -Namespace root\Subscription -Class __EventConsumer")
+    
+        Invoke-Command -ComputerName $Computer -ScriptBlock $ScriptBlock_Consumer
+
+        # Collect WMI Event to Consumer Bindings
+        $ScriptBlock_Binding = $ExecutionContext.InvokeCommand.NewScriptBlock("Get-WMIObject -Namespace root\Subscription -Class __FilterToConsumerBinding")
+    
+        Invoke-Command -ComputerName $Computer -ScriptBlock $ScriptBlock_Binding
+
+
+    }
+
+
+}
