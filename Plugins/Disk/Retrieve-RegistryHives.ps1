db453c4034efb15ddf971a60f17ddd01ac861029
5ynax <41930323+5ynax@users.noreply.github.com>
2019-04-05 22:47:11 -0500

    All the plugins! (#268)

--- Plugins/Disk/Retrieve-RegistryHives.ps1
+++ Plugins/Disk/Retrieve-RegistryHives.ps1
@@ -1,22 +1,14 @@
-﻿<#
+<#
 
 .SYNOPSIS
-    Plugin-Name: Retrieve-WindowsArtifacts.ps1
+    Plugin-Name: Retrieve-RegistryHives.ps1
     
 .Description
-    This plugin collects windows artifacts (including locked files) from the 
-    preset list below. The file are copied by pushing the Velociraptor binary to 
-    the the remote system, where it copies the files to C:\ProgramData\%COMPUTERNAME%.
-    7za.exe is also copied to the system, to then zip the directory of artifacts 
-    before moving them back to your local system for further analysis. This plugin 
-    will remove the Velociraptor, 7zip PE, and all locally created files after 
-    successfully pulling the artifacts back to the output destination in Power-Response.
+    This plugin retrives system and user registry hives from a remote machine and 
+    copies them back to the analysis machine for further review.
 
-    System Artifacts:
-    %SystemDrive%\$MFT
-    %SYSTEMROOT%\Tasks
-    %SYSTEMROOT%\System32\Tasks
-    %SYSTEMROOT%\Prefetch
+    Collected registry hives:
+
     %SYSTEMROOT%\System32\config\SAM
     %SYSTEMROOT%\System32\config\SAM.LOG1
     %SYSTEMROOT%\System32\config\SAM.LOG2
@@ -29,23 +21,13 @@
     %SYSTEMROOT%\System32\config\SECURITY
     %SYSTEMROOT%\System32\config\SECURITY.LOG1
     %SYSTEMROOT%\System32\config\SECURITY.LOG2
-    %SYSTEMROOT%\Appcompat\Programs
-    %SYSTEMROOT%\System32\drivers\etc\hosts
-    %SYSTEMROOT%\System32\winevt\logs
-    %PROGRAMDATA%\Microsoft\Search\Data\Applications\Windows
-    %PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 
-    User Artifacts:
     %UserProfile%\NTUSER.DAT
     %UserProfile%\NTUSER.DAT.LOG1
     %UserProfile%\NTUSER.DAT.LOG2
     %UserProfile%\AppData\Local\Microsoft\Windows\UsrClass.dat
     %UserProfile%\AppData\Local\Microsoft\Windows\UsrClass.dat.LOG1
     %UserProfile%\AppData\Local\Microsoft\Windows\UsrClass.dat.LOG2
-    %UserProfile%\AppData\Roaming\Microsoft\Windows\Recent
-    %UserProfile%\AppData\Local\Google\Chrome\User Data\Default\History
-    %UserProfile%\AppData\Local\Microsoft\Windows\WebCache
-    %UserProfile%\AppData\Roaming\Mozilla\Firefox\Profiles\*.default\places.sqlite
 
 .EXAMPLE
 
@@ -55,13 +37,13 @@
     Run
 
 .NOTES
-    Author: Matt Weikert
-    Date Created: 2/12/2019
-    Twitter: @5k33tz
-    
-    Last Modified By: Drew Schmitt
-    Last Modified Date: 4/5/2019
+    Author: Drew Schmitt
+    Date Created: 4/5/2019
     Twitter: @5ynax
+    
+    Last Modified By: 
+    Last Modified Date: 
+    Twitter: 
   
 #>
 
@@ -109,7 +91,7 @@ process{
     }
 
     # Set $Output for where to store recovered artifacts
-    $Output= (Get-PROutputPath -ComputerName $Session.ComputerName -Directory 'Artifacts')
+    $Output= (Get-PROutputPath -ComputerName $Session.ComputerName -Directory 'RegistryHives')
 
     # Create Subdirectory in $global:PowerResponse.OutputPath for storing artifacts
     If (!(Test-Path $Output)){
@@ -193,10 +175,6 @@ process{
     #Collect System Artifacts    
     $SystemArtifacts = @(
 
-        "$env:SystemDrive\```$MFT",
-        "$env:SystemRoot\Tasks\*",
-        "$env:SystemRoot\System32\Tasks\*",
-        "$env:SystemRoot\Prefetch\*",
         "$env:SystemRoot\System32\config\SAM",
         "$env:SystemRoot\System32\config\SAM.LOG1",
         "$env:SystemRoot\System32\config\SAM.LOG2",
@@ -208,13 +186,7 @@ process{
         "$env:SystemRoot\System32\config\SOFTWARE.LOG2",
         "$env:SystemRoot\System32\config\SECURITY",
         "$env:SystemRoot\System32\config\SECURITY.LOG1",
-        "$env:SystemRoot\System32\config\SECURITY.LOG2",
-        "$env:SystemRoot\Appcompat\Programs\*",
-        "$env:SystemRoot\System32\drivers\etc\hosts",
-        "$env:SystemRoot\System32\winevt\logs\*",
-        "$env:SystemRoot\system32\sru\SRUDB.dat",
-        "$env:ProgramData\Microsoft\Search\Data\Applications\Windows\*",
-        "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\*"
+        "$env:SystemRoot\System32\config\SECURITY.LOG2"
 
     )
            
@@ -233,16 +205,12 @@ process{
         "NTUSER.DAT.LOG2",
         "AppData\Local\Microsoft\Windows\UsrClass.dat",
         "AppData\Local\Microsoft\Windows\UsrClass.dat.LOG1",
-        "AppData\Local\Microsoft\Windows\UsrClass.dat.LOG2",
-        "AppData\Roaming\Microsoft\Windows\Recent*\*",
-        "AppData\Local\Google\Chrome\User*\Default\History*",
-        "AppData\Local\Microsoft\Windows\WebCache\*",
-        "AppData\Roaming\Mozilla\Firefox\Profiles\*.default\places.sqlite"
+        "AppData\Local\Microsoft\Windows\UsrClass.dat.LOG2"
 
         )
 
     # Grab list of user profiles
-    $Users = Invoke-Command -Session $Session -Scriptblock {Get-CimInstance -ClassName Win32_UserProfile | Select-Object -ExpandProperty LocalPath | Select-String Users} | Out-Null
+    $Users = Invoke-Command -Session $Session -Scriptblock {Get-CimInstance -ClassName Win32_UserProfile | Select-Object -ExpandProperty LocalPath | Select-String Users}
 
     # Iterate through each user profile grabbing the artifacts
     foreach ($User in $Users){
@@ -256,13 +224,13 @@ process{
     }
         
     # Compress artifacts directory      
-    $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("& C:\ProgramData\{0} a C:\ProgramData\{1}_Artifacts.zip C:\ProgramData\{1}") -f ((Split-Path $Installexe -Leaf), $Session.ComputerName))
+    $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("& C:\ProgramData\{0} a C:\ProgramData\{1}_RegistryHives.zip C:\ProgramData\{1}") -f ((Split-Path $Installexe -Leaf), $Session.ComputerName))
     Invoke-Command -Session $Session -ScriptBlock $ScriptBlock -ErrorAction SilentlyContinue | Out-Null
 
     # Copy artifacts back to $Output (Uses $Session)
     try {
 
-        Copy-Item -Path (("C:\ProgramData\{0}_Artifacts.zip") -f ($Session.ComputerName)) -Destination "$Output\" -FromSession $Session -Force -ErrorAction Stop
+        Copy-Item -Path (("C:\ProgramData\{0}_RegistryHives.zip") -f ($Session.ComputerName)) -Destination "$Output\" -FromSession $Session -Force -ErrorAction Stop
 
     } catch {
 
@@ -270,7 +238,7 @@ process{
     }
     
     # Delete initial artifacts, 7za, and velociraptor binaries from remote machine
-    $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("Remove-Item -Force -Recurse -Path C:\ProgramData\{0}, C:\ProgramData\{1}, C:\ProgramData\{2}_Artifacts.zip, C:\ProgramData\{2}") -f ((Split-Path $Velo_exe -Leaf), (Split-Path $Installexe -Leaf), $Session.ComputerName))
+    $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("Remove-Item -Force -Recurse -Path C:\ProgramData\{0}, C:\ProgramData\{1}, C:\ProgramData\{2}_RegistryHives.zip, C:\ProgramData\{2}") -f ((Split-Path $Velo_exe -Leaf), (Split-Path $Installexe -Leaf), $Session.ComputerName))
     Invoke-Command -Session $Session -ScriptBlock $ScriptBlock | Out-Null
 
 }
