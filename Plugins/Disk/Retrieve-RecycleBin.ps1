ea899eb6288fe61b85b93097a91e93d03a901a04
valrkey <10157559+valrkey@users.noreply.github.com>
2019-09-25 17:47:23 -0500

    Removed Velociraptor

--- Plugins/Disk/Retrieve-RecycleBin.ps1
+++ Plugins/Disk/Retrieve-RecycleBin.ps1
@@ -47,14 +47,6 @@ process{
     $7za32 = ("{0}\7za_x86.exe" -f (Get-PRPath -Bin))
     $7za64 = ("{0}\7za_x64.exe" -f (Get-PRPath -Bin))
 
-    #Velociraptor checks
-    $VeloTestPath = "C:\ProgramData\Velociraptor*.exe"
-    $VeloFlag = Invoke-Command -Session $Session -ScriptBlock {Test-Path $($args[0])} -ArgumentList $VeloTestPath
-
-    #Velociraptor BIN locations
-    $Velo_64 = ("{0}\Velociraptor_x64.exe" -f (Get-PRPath -Bin))
-    $Velo_32 = ("{0}\Velociraptor_x86.exe" -f (Get-PRPath -Bin))
-
     if (!$7zFlag){
 
         # Verify that 7za executables are located in (Get-PRPath -Bin)
@@ -69,23 +61,6 @@ process{
         } elseif (!$7z32bitTestPath) {
 
             Throw "32 bit version of 7za.exe not detected in Bin. Place 32bit executable in Bin directory and try again."
-        }
-    }
-
-    if (!$VeloFlag){
-
-        #Verify that Velociraptor executables are located in (Get-PRPath -Bin) (For locked files)
-
-        $Velo_64TestPath = Get-Item -Path $Velo_64 -ErrorAction SilentlyContinue
-        $Velo_32TestPath = Get-Item -Path $Velo_32 -ErrorAction SilentlyContinue
-
-        if (!$Velo_64TestPath) {
-
-            Throw "64 bit version of Velociraptor not detected in Bin. Place 64bit executable in Bin directory and try again."
-
-        } elseif (!$Velo_32TestPath) {
-
-            Throw "32 bit version of Velociraptor not detected in Bin. Place 32bit executable in Bin directory and try again."
         }
     }
 
@@ -106,12 +81,10 @@ process{
         if ($Architecture -eq "64-bit") {
 
             $Installexe = $7za64
-            $Velo_exe = $Velo_64
 
         } elseif ($Architecture -eq "32-bit") {
 
             $Installexe = $7za32
-            $Velo_exe = $Velo_32
 
         } else {
         
@@ -140,20 +113,6 @@ process{
 
     }
 
-    
-    if (!$VeloFlag){
-
-        try {
-
-            Copy-Item -Path $Velo_exe -Destination "C:\ProgramData" -ToSession $Session -Force -ErrorAction Stop
-
-        } catch {
-
-            Throw "Could not copy Velociraptor to remote machine. Quitting..."
-        }
-
-    }
-
     #Create Output directory structure on remote host
     $TestRemoteDumpPath = Invoke-Command -Session $Session -ScriptBlock {Get-Item -Path ("C:\ProgramData\{0}" -f $($args[0])) -ErrorAction SilentlyContinue} -ArgumentList $Session.ComputerName
 
@@ -164,13 +123,10 @@ process{
     }
 
     #Collect System Artifacts    
-    $SIDS = Get-ChildItem -Path 'C:\$Recycle.Bin' -Force | Select -ExpandProperty Name
-           
-    foreach ($SID in $SIDS){
+    $Path = Get-ChildItem -Path 'C:\$Recycle.Bin' -Force | Select -ExpandProperty FullName | Foreach-Object { Join-Path -Path $PSItem -ChildPath '*' }
 
-        $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("& 'C:\ProgramData\{0}' fs --accessor ntfs cp \\.\C:\```$Recycle.Bin\{1}\* C:\ProgramData\{2}") -f ((Split-Path $Velo_exe -Leaf), $SID, $Session.ComputerName))
-        Invoke-Command -Session $Session -ScriptBlock $ScriptBlock -ErrorAction SilentlyContinue | Out-Null
-    }
+    #Stage System Artifacts
+    Copy-PRItem -Session $Session -Path $Path -Destination (Join-Path -Path 'C:\ProgramData' -ChildPath $Session.ComputerName)
         
     # Compress artifacts directory      
     $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("& 'C:\ProgramData\{0}' a C:\ProgramData\{1}_RecycleBin.zip C:\ProgramData\{1}") -f ((Split-Path $Installexe -Leaf), $Session.ComputerName))
@@ -192,14 +148,7 @@ process{
         $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("Remove-Item -Force -Recurse -Path C:\ProgramData\{0}") -f (Split-Path $Installexe -Leaf))
         Invoke-Command -Session $Session -ScriptBlock $ScriptBlock | Out-Null
     }
-    
-    #Delete Velociraptor if deployed by plugin
-    if (!$VeloFlag){
 
-        $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("Remove-Item -Force -Recurse -Path C:\ProgramData\{0}") -f (Split-Path $Velo_exe -Leaf))
-        Invoke-Command -Session $Session -ScriptBlock $ScriptBlock | Out-Null
-    }
-    
     # Delete reamining artifacts from remote machine
     $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("Remove-Item -Force -Recurse -Path C:\ProgramData\{0}_RecycleBin.zip, C:\ProgramData\{0}") -f ($Session.ComputerName))
     Invoke-Command -Session $Session -ScriptBlock $ScriptBlock | Out-Null
