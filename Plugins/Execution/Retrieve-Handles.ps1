02ac55d527f2ae0d08d5c390eb2a4258c050a6dc
5ynax <5ynax@5ynax.com>
2019-10-08 12:11:00 -0500

    refactor retrieve-handles

--- Plugins/Execution/Retrieve-Handles.ps1
+++ Plugins/Execution/Retrieve-Handles.ps1
@@ -19,73 +19,122 @@
     Date Created: 3/16/2019
     Twitter: @5ynax
     
-    Last Modified By:
-    Last Modified Date:
-    Twitter:
+    Last Modified By: Drew Schmitt
+    Last Modified Date: 10/08/2019
+    Twitter: @5ynax
   
 #>
-
+[cmdletbinding()]
 param (
 
     [Parameter(Mandatory=$true,Position=0)]
-    [System.Management.Automation.Runspaces.PSSession]$Session
+    [System.Management.Automation.Runspaces.PSSession[]]$Session
 
-    )
+)
 
 process {
 
-	#Verify that bin dependencies are met
-    $Bin_32 = ("{0}\handle.exe" -f (Get-PRPath -Bin))
-    $Bin_64 = ("{0}\handle64.exe" -f (Get-PRPath -Bin))
+    # Get stage directory
+    $RemoteStageDirectory = Get-PRConfig -Property 'RemoteStagePath'
 
-    if (!(Test-Path $Bin_32)){
-
-        Throw "handle.exe not found in {0}. Place executable in binary directory and try again." -f (Get-PRPath -Bin)
+    # Define $Dependency tracking structure
+    $Dependency = [Ordered]@{
+        Handles = @{
+            Command = '$Raw = Invoke-Expression "<DEPENDENCYPATH> -accepteula" | Select-Object -Skip 5; $Handles = $Raw | Select-Object -Property @{ N = "Process"; E = { ($_ -split ''(?<!:)\s+'')[0] } }, @{ N = "ID"; E = { (($_ -split ''(?<!:)\s+'')[1] -replace ''pid:\s*'') } }, @{ N = "Type"; E = { ($_ -split ''(?<!:)\s+'')[2] -replace ''type:\s*'' } }, @{ N = "Path"; E = { ($_ -split ''(?<!:)\s+'')[3] -replace ''^[^ ]*: '' } }; [PSCustomObject]$Handles'
+            Path = @{
+                '32-bit' = Join-Path -Path (Get-PRPath -Bin) -ChildPath 'Handle.exe'
+                '64-bit' = Join-Path -Path (Get-PRPath -Bin) -ChildPath 'Handle64.exe'
+            }
+            TestPath = (Join-Path -Path $RemoteStageDirectory -ChildPath 'Handle*.exe')
+        }
     }
 
-    if (!(Test-Path $Bin_64)){
+    # Begin dependency deploy logic
+    # Verify the each $Dependency exe exists
+    $Dependency | Select-Object -ExpandProperty 'Keys' -PipelineVariable 'Dep' | Foreach-Object { $Dependency.$Dep.Path.GetEnumerator() | Where-Object { !(Test-Path -Path $PSItem.Value -PathType 'Leaf') } | Foreach-Object { throw ('{0} version of {1} not detected in Bin. Place {0} executable in Bin directory and try again.' -f $PSItem.Key,$Dep) } }
 
-        Throw "handle64.exe not found in {0}. Place executable in binary directory and try again." -f (Get-PRPath -Bin)
+    foreach ($Key in $Dependency.Keys) {
+        # Track all session we are deploying this dependency to
+        $Dependency.$Key.Deploy = Invoke-Command -Session $Session -ScriptBlock { $Key = $using:Key; $Dependency = $using:Dependency; (Get-Item -Force -Path $Dependency.$Key.TestPath -ErrorAction 'SilentlyContinue' | Select-Object -First 1) -eq $null } | Where-Object { $PSItem } | Foreach-Object { Get-PSSession -InstanceId $PSItem.RunspaceId }
+
+        foreach ($Instance in $Dependency.$Key.Deploy) {
+            try {
+                # Determine system $Architecture and select proper executable
+                $Architecture = Invoke-Command -Session $Instance -ScriptBlock { if (!(Test-Path -Path $using:RemoteStageDirectory -PathType 'Container')) { $null = New-Item -Path $using:RemoteStageDirectory -ItemType 'Directory' }; Get-WmiObject -Class 'Win32_OperatingSystem' -Property 'OSArchitecture' -ErrorAction 'Stop' | Select-Object -ExpandProperty 'OSArchitecture' }
+            } catch {
+                # Unable to get $Architecture information
+                $Warning = 'Unable to determine system architecture for {0}. Data was not gathered.' -f $Instance.ComputerName
+            }
+
+            # Ensure we are tracking a sensible $Architecture
+            if ($Architecture -and $Dependency.$Key.Path.Keys -NotContains $Architecture) {
+                $Warning = 'Unknown system architecture ({0}) detected for {1}. Data was not gathered.)' -f $Architecture, $Instance.ComputerName
+            }
+
+            # If we ran into problems with the above checks
+            if ($Warning) {
+                # Write the warning
+                Write-PRWarning -Message $Warning
+
+                # Remove the failed $Session for master and deploy list
+                $Session = $Session | Where-Object { $PSItem.ComputerName -ne $Instance.ComputerName }
+                $Dependency.$Key.Deploy = $Dependency.$Key.Deploy | Where-Object { $PSItem.ComputerName -ne $Instance.ComputerName }
+
+                # Continue to next item
+                continue
+            }
+
+            # Compute the $RemoteDependency path
+            $RemoteDependency = Join-Path -Path $RemoteStageDirectory -ChildPath (Split-Path -Leaf -Path $Dependency.$Key.Path.$Architecture)
+
+            try {
+                # Copy dependency executable to the remote machine
+                Copy-Item -Path $Dependency.$Key.Path.$Architecture -Destination $RemoteDependency -ToSession $Instance -Force -ErrorAction 'Stop'
+            } catch {
+                # Failed to copy dependency
+                throw ('Could not copy {0} to remote machine. Quitting.' -f $Key)
+            }
+        }
     }
+    # End dependency deploy logic
 
-    #Determine remote system architecture
-    $OSArchitecture = Invoke-Command -Session $Session -ScriptBlock {(Get-WmiObject Win32_OperatingSystem).OSArchitecture}
-
-    #Select the proper EXE for OS Architecture
-    if ($OSArchitecture -eq "64-bit") {
-
-        $LocalExe = $Bin_64
-        $RemoteExe = "handle64.exe"
-    }
-
-    if ($OSArchitecture -eq "32-bit") {
-
-        $LocalExe = $Bin_32
-        $RemoteExe = "handle.exe"
-    }
-
-    #Copy binary to remote machine for execution and data collection
-    try {
-
-        Copy-Item -Path $LocalExe -Destination "C:\ProgramData\$RemoteExe" -ToSession $Session -Force -ErrorAction Stop
-
-    } catch {
-
-        throw "Error copying the binary to the remote maching. Quitting."
-    }
+    # Start plguin logic
 
     #Collect Data from the remote machine
 
-    $HandlesRaw = Invoke-Command -Session $Session -ScriptBlock {Invoke-Expression -Command "C:\ProgramData\$($args[0]) -accepteula | Select-Object -Skip 5"} -ArgumentList $RemoteExe
+    Invoke-Command -Session $Session -Scriptblock {
+        # Pull remote Dependency into a local variable in each session
+        $Dependency = $using:Dependency
 
-    #Format data for ingestion into readable Format
+        # Get all non-exe paths in the remote stage directory
+        $Path = Get-ChildItem -Force -Path $using:RemoteStageDirectory -Exclude '*.exe' | Select-Object -ExpandProperty 'FullName'
 
-    $Handles = $HandlesRaw | Select-Object -Property @{ N = "Process"; E = { ($_ -split '(?<!:)\s+')[0] } }, @{ N = "ID"; E = { (($_ -split '(?<!:)\s+')[1] -replace 'pid:\s*') } }, @{ N = "Type"; E = { ($_ -split '(?<!:)\s+')[2] -replace 'type:\s*' } }, @{ N = "Path"; E = { ($_ -split '(?<!:)\s+')[3] -replace '^[^ ]*: ' } }
-                                               
-    [PSCustomObject]$Handles
+        foreach ($Key in $Dependency.Keys) {
+            # Get actual dependency path
+            $DependencyPath = Get-Item -Path $Dependency.$Key.TestPath | Select-Object -First 1 -ExpandProperty 'FullName'
 
-    #Remove handles binary
+            # Create dependency command
+            $Command = $Dependency.$Key.Command -Replace '<DEPENDENCYPATH>',$DependencyPath -Replace '<PATH>',($Path -Join ' ')
 
-    Invoke-Command -Session $Session -ScriptBlock {Remove-Item -Path "C:\ProgramData\$($args[0])" -Force} -ArgumentList $RemoteExe
-                                        
+            # Execute dependency command
+            Invoke-Expression -Command $Command
+        }
+    }
+    
+    # End plugin logic
+
+    # Remove $Dependency if deployed by this plugin
+    $Dependency.Keys | Where-Object { $Dependency.$PSItem.Deploy } | Foreach-Object { Invoke-Command -Session $Dependency.$Key.Deploy -ScriptBlock { $Key = $using:Key; $Dependency = $using:Dependency; Remove-Item -Force -Path $Dependency.$Key.TestPath } }
+         
+    # Remove created files on remote machine as cleanup
+    Invoke-Command -Session $Session -ScriptBlock {
+        # By default remove entire remote stage directory
+        $RemovePath = $using:RemoteStageDirectory
+
+        # Unless we have deployed exes there
+        Get-ChildItem -Force -Path $using:RemoteStageDirectory -Include '*.exe' | Select-Object -First 1 | Foreach-Object { $RemovePath = $Path }
+
+        # Remove the archive
+        Remove-Item -Force -Recurse -Path $RemovePath
+    }                               
 }
