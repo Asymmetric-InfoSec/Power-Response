fd250e17232c95f1b70bae270ca40fe9aa63d317
5ynax <41930323+5ynax@users.noreply.github.com>
2019-10-08 13:45:46 -0500

    Refactored Prefetch (#418)
    * refactored prefetch
    
    * deploy fix

--- Plugins/Analysis/Analyze-Prefetch.ps1
+++ Plugins/Analysis/Analyze-Prefetch.ps1
@@ -6,9 +6,7 @@
 .Description
     Analyzes recovered prefetch files for all hosts that you have collected data from.
     There are checks built in to not analyze twice. By default, the plugin will look for 
-    prefetch results from the current date. You can specify the analysis date with the
-    $AnalyzeDate parameter. When using the $AnalyzeDate parameter, you must put your
-    date in the format of yyyyMMdd.
+    prefetch results from the current date.
 
     Dependencies
     PECmd.exe (From Eric Zimmerman's Tools. stored in the Power-Response Bin directory)
@@ -29,66 +27,82 @@
     Date Created: 3/9/2019
     Twitter: @5ynax
     
-    Last Modified By:
-    Last Modified Date:
-    Twitter:
+    Last Modified By: Drew Schmitt
+    Last Modified Date: 10/04/2019
+    Twitter:@5ynax
   
 #>
 
 param (
 
     [Parameter(Mandatory=$false,Position=0)]
-    [System.Management.Automation.Runspaces.PSSession[]]$Session, 
+    [System.Management.Automation.Runspaces.PSSession[]]$Session 
 
-    [Parameter(Mandatory=$false,Position=1)]
-    [DateTime]$AnalyzeDate= (Get-Date)
-
-    )
+)
 
 process{
 
-    #Format String Properly for use
-    $AnalysisDate = ('{0:yyyyMMdd}' -f $AnalyzeDate)
+     # Get the plugin name
+    $PluginName = $MyInvocation.MyCommand.Name -Replace '.+-' -Replace '\..+'
 
-    #Verify that bin dependencies are met
-    $TestBin = Test-Path ("{0}\PEcmd.exe" -f (Get-PRPath -Bin))
+    # Get encryption password
+    $EncryptPassword = Get-PRConfig -Property 'EncryptPassword'
 
-    if (!$TestBin){
+    # Get system architecture
+    $Architecture = Get-CimInstance -Class 'Win32_OperatingSystem' -Property 'OSArchitecture' | Select-Object -ExpandProperty 'OSArchitecture'
 
-        Throw "PECmd not found in {0}. Place executable in binary directory and try again." -f (Get-PRPath -Bin)
-    }
-
-    #Build list of hosts that have been analyzed with Power-Response
-    $Machines = Get-ChildItem (Get-PRPath -Output)
-
-    #Loop through and analyze prefetch files, while skipping if the analysis directory exists
-    foreach ($Machine in $Machines){
-        
-        #Path to verify for existence before processing prefetch
-        $PrefetchPath = ("{0}\{1}\Execution\Prefetch_{2}") -f (Get-PRPath -Output), $Machine, $AnalysisDate 
-        
-        #Determine if prefetch output directory exists
-        if (Test-Path $PrefetchPath){
-
-            #Verify that prefetch has not already been analyzed
-            $PrefetchProcessed = "$PrefetchPath\Analysis"
-
-            if (!(Test-Path $PrefetchProcessed)) {
-
-                #Create Analysis Directory
-                New-Item -Type Directory -Path $PrefetchProcessed | Out-Null
-
-                #Process Prefetch and store in analysis directory
-                $Command = ("& '{0}\PECmd.exe' -d {1} --csv {2}") -f (Get-PRPath -Bin),$PrefetchPath,$PrefetchProcessed
-
-                Invoke-Expression -Command $Command | Out-File -FilePath ("{0}\PECmd_Log.txt" -f $PrefetchProcessed)
-
-            } else {
-
-                #Prevent additional processing of prefetch already analyzed
-                continue
+    # Define $Dependency tracking structure
+    $Dependency = [Ordered]@{
+        SevenZip = @{
+            Command = '& "<DEPENDENCYPATH>" x -p{0} <ZIPPATH> -o<OUTPUTPATH>' -f $EncryptPassword
+            Path = @{
+                '32-bit' = Join-Path -Path (Get-PRPath -Bin) -ChildPath '7za_x86.exe'
+                '64-bit' = Join-Path -Path (Get-PRPath -Bin) -ChildPath '7za_x64.exe'
+            }
+        }
+        PECmd = @{
+            Command = '& "<DEPENDENCYPATH>" -d <ARTIFACTPATH> --csv <ANALYSISFOLDERPATH>'
+            Path = @{
+                '32-bit' = Join-Path -Path (Get-PRPath -Bin) -ChildPath 'PECmd.exe'
+                '64-bit' = Join-Path -Path (Get-PRPath -Bin) -ChildPath 'PECmd.exe'
             }
         }
     }
-}
 
+    # Verify the each $Dependency exe exists
+    $Dependency | Select-Object -ExpandProperty 'Keys' -PipelineVariable 'Dep' | Foreach-Object { $Dependency.$Dep.Path.GetEnumerator() | Where-Object { !(Test-Path -Path $PSItem.Value -PathType 'Leaf') } | Foreach-Object { throw ('{0} version of {1} not detected in Bin. Place {0} executable in Bin directory and try again.' -f $PSItem.Key,$Dep) } }
+
+    # Figure out which folders to process
+    $ToProcess = Get-ChildItem -Recurse -Force -Path (Get-PRPath -Output) | Where-Object { $PSItem.Name -Match $PluginName -and $PSItem.Name -NotMatch 'Analysis' -and $PSItem.PSIsContainer } | Where-Object { !(Get-ChildItem -Path $PSItem.FullName | Where-Object { $PSItem.Name -Match 'Analysis' }) }
+
+    foreach ($ArtifactDirectory in $ToProcess) {
+        # Get the zip file
+        $ZipPath = Get-ChildItem -File -Force -Path $ArtifactDirectory.FullName | Where-Object { $PSItem.Name -Match '.+\.zip' } | Select-Object -First 1 -ExpandProperty 'FullName'
+
+        # Build command
+        $Command = $Dependency.SevenZip.Command -Replace '<DEPENDENCYPATH>',$Dependency.SevenZip.Path.$Architecture -Replace '<ZIPPATH>',$ZipPath -Replace '<OUTPUTPATH>',$ArtifactDirectory.FullName
+
+        # Run the command
+        $null = Invoke-Expression -Command $Command
+
+        # Get the unzipped artifact paths
+        $Artifacts = Get-ChildItem -Force -Directory -Path $ArtifactDirectory.FullName | Where-Object { $PSItem.Name -eq 'C' -and $PSItem.PSIsContainer }
+
+        # Naming convention {DIRNAME}_Analysis
+        $AnalysisDirectoryName = '{0}_Analysis' -f $ArtifactDirectory.Name
+
+        # Build Analysis directory
+        $AnalysisDirectory = Join-Path -Path $ArtifactDirectory.FullName -ChildPath $AnalysisDirectoryName | Foreach-Object { New-Item -Type 'Directory' -Path $PSItem }
+
+        foreach ($ArtifactPath in $Artifacts) {
+            # Build the log file
+            $LogFile = Join-Path -Path $AnalysisDirectory.FullName -ChildPath ('PECmd_{0}_Log.txt' -f $ArtifactPath.Name)
+
+            # Build the command
+            $Command = $Dependency.PECmd.Command -Replace '<DEPENDENCYPATH>',$Dependency.PECmd.Path.$Architecture -Replace '<ARTIFACTPATH>',$ArtifactPath.FullName -Replace '<ANALYSISFOLDERPATH>',$AnalysisDirectory.FullName
+
+            # Run the command
+            Invoke-Expression -Command $Command -ErrorAction 'SilentlyContinue' | Out-File -FilePath $LogFile
+        }
+    }
+}
