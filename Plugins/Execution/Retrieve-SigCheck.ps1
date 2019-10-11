3b1882c20333aa1469bab39a37a901c38c24f101
5ynax <5ynax@5ynax.com>
2019-10-11 14:27:50 -0500

    refactor sigcheck, autoruns, memory and fls fix

--- Plugins/Execution/Retrieve-SigCheck.ps1
+++ Plugins/Execution/Retrieve-SigCheck.ps1
@@ -9,17 +9,19 @@
 
     This execution is equivalent to executing: 
 
-    Sigcheck.exe /accepteula -nobanner -e -vt -h -c -s [location]
+    Sigcheck.exe /accepteula -nobanner -e [-vt] -h -c -s <location>
 
     The options that are inlcuded in this execution are:
 
     -e             Executables only (regardless of extension)
-    -vt            Submit to VirusTotal (hashes only)
     -h             Show hashes
     -c             Output as CSV (comma delmited)
     -s             Recursive subdirectories (if utilizing the $Recurse switch)
     -nobanner      Does not inlcude Autoruns banner
     /accepteula    Does not prompt for EULA acceptance
+
+    Note: You can add in the -vt flag into the commands below if you want to submit
+    hashes to VT for analysis. Consider your OPSEC plans before adding this in.
 
     Dependencies:
 
@@ -47,115 +49,179 @@
     Date Created: 1/2/2019
     Twitter: @5ynax
     
-    Last Modified By:
-    Last Modified Date:
-    Twitter:
+    Last Modified By: Drew Schmitt
+    Last Modified Date: 10/11/2019
+    Twitter: @5ynax
   
 #>
 
 param (
 
     [Parameter(Mandatory=$true,Position=0)]
-    [System.Management.Automation.Runspaces.PSSession]$Session,
+    [System.Management.Automation.Runspaces.PSSession[]]$Session,
 
-    [Parameter(Mandatory=$false,Position=1)]
-    [string]$Location = 'C:\Windows',
+    [Parameter(Mandatory=$true,Position=1)]
+    [string]$Location,
 
     [Parameter(Mandatory=$false,Position=2)]
-    [Switch]$Recurse
+    [Switch]$Recurse,
 
-    )
+    [Parameter(Position=1)]
+    [Switch]$Force
+
+)
 
 process{
 
-    # Sigcheck executable locations
-    $Sigcheck64 = ("{0}\sigcheck64.exe" -f (Get-PRPath -Bin))
-    $Sigcheck32 = ("{0}\sigcheck.exe" -f (Get-PRPath -Bin))
+     # Get the plugin name
+    $PluginName = $MyInvocation.MyCommand.Name -Replace '\..+'
 
-    # Verify binaries exist in Bin
-    $64bitTestPath = Get-Item -Path $Sigcheck64 -ErrorAction SilentlyContinue
-    $32bitTestPath = Get-Item -Path $Sigcheck32 -ErrorAction SilentlyContinue
+    # Remote archive name with second count for randomness
+    $PluginOutputName = '{0}_{1}' -f ($PluginName -Replace '.+-'),(Get-Date -UFormat %s).Split('.')[0]
 
-    if (!$64bitTestPath) {
+    $SessionCopy = $Session
 
-        Throw "Sigcheck64.exe not detected in Bin. Place 64bit executable in Bin directory and try again."
+    # If we are not forcing through, see if we have collected artifacts already
+    if (!$Force) {
+        # Loop through each session computer name
+        foreach ($ComputerName in $Session.ComputerName) {
+            # Determine where the plugin log is
+            $LogPath = Join-Path (Get-PRPath -Output) -ChildPath $ComputerName | Join-Path -ChildPath ('{0}_plugin-log.csv' -f $ComputerName) 
 
-    } elseif (!$32bitTestPath) {
+            # Check if this plugin has already been executed today
+            Import-Csv -Path $LogPath -ErrorAction 'SilentlyContinue' | Where-Object { $PSItem.Success -and $PSItem.Plugin -eq $PluginName -and [DateTime]$PSItem.Date -gt (Get-Date).ToUniversalTime().Date } | Select-Object -First 1 | Foreach-Object {
+                # Write warning message to use Force parameter
+                Write-PRWarning -Message ("Plugin {0} has already been executed for system {1}. If you want to execute it again, use the 'Force' parameter" -f $PluginName,$ComputerName)
 
-        Throw "Sigcheck.exe not detected in Bin. Place 32bit executable in Bin directory and try again."
-
-    }
-
-    # Determine system architecture and select proper Sigcheck executable
-    try {
-
-        $Architecture = Invoke-Command -Session $Session -ScriptBlock {(Get-WmiObject -Class Win32_OperatingSystem -Property OSArchitecture -ErrorAction Stop).OSArchitecture}
-        
-        if ($Architecture -eq "64-bit") {
-
-            $Installexe = $Sigcheck64
-
-        } elseif ($Architecture -eq "32-bit") {
-
-            $Installexe = $Sigcheck32
-
-        } else {
-            
-            Write-Error ("Unknown system architecture ({0}) detected for {1}. Data was not gathered.)" -f $Architecture, $Session.ComputerName)
-            Continue
+                # Remove already executed session from tracked session list
+                $SessionCopy = $SessionCopy | Where-Object { $PSItem.ComputerName -ne $ComputerName }
+            }
         }
-    } catch {
-        
-        Write-Error ("Unable to determine system architecture for {0}. Data was not gathered." -f $Session.ComputerName)
-        Continue
     }
 
-    # Copy Sigcheck to remote host
-    $RemotePath = ("C:\ProgramData\{0}") -f (Split-Path $Installexe -Leaf)
-
-    try {
-        
-        Copy-Item -Path $Installexe -Destination $RemotePath -ToSession $Session -ErrorAction Stop
-        
-        $RemoteFile = Invoke-Command -Session $Session -ScriptBlock {Get-Item -Path $($args[0]) -ErrorAction Stop} -Argumentlist $RemotePath
-
-        # verify that the file copy succeeded to the remote host
-        if (!$RemoteFile) {
-            
-            Write-Error ("Remote file not found on {0}. There may have been a problem during the copy process. Data was not gathered." -f $Session.ComputerName)
-            Continue
-        }
-
-    } catch {
-        
-        Write-Error ("An unexpected error occurred while copying Sigcheck to {0}. Data was not gathered" -f $Session.ComputerName)
-        Continue
-    }
-
-    # Run Sigcheck on the remote host and collect Sigcheck data (use -s option if $Recurse -eq $true)
-    if ($Recurse) {
-    
-        $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("& '{0}' /accepteula -nobanner -e -vt -h -c -s {1}") -f ($RemotePath, $Location))
-    
-        Invoke-Command -Session $Session -ScriptBlock $ScriptBlock
-
+    # If we don't have any sessions left, return
+    if ($SessionCopy) {
+        $Session = $SessionCopy
     } else {
-
-         $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("& '{0}' /accepteula -nobanner -e -vt -h -c {1}") -f ($RemotePath, $Location))
-    
-        Invoke-Command -Session $Session -ScriptBlock $ScriptBlock | ConvertFrom-CSV
-
+        return
     }
-    
-    # Remove Sigcheck from remote host
-    try {
-            
-            $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("Remove-Item {0} -Force -ErrorAction Stop") -f ($RemotePath))
-            Invoke-Command -Session $Session -ScriptBlock $ScriptBlock
-            
-        } catch {
-            
-            Write-Error ("Unable to remove the Sigcheck executable from {0}. The file will need to be removed manually." -f $Session.ComputerName)
-            Continue
+
+    # Get stage directory
+    $RemoteStageDirectory = Get-PRConfig -Property 'RemoteStagePath'
+
+    # Get encryption password
+    $EncryptPassword = Get-PRConfig -Property 'EncryptPassword'
+
+    # Remote archive name
+    $Archive = (Join-Path -Path $RemoteStageDirectory -ChildPath $PluginOutputName) + '.zip'
+
+    # Define $Dependency tracking structure
+    $Dependency = [Ordered]@{
+        SigCheck = @{
+            Command = @{
+                'NoRecurse' = '& "<DEPENDENCYPATH>" /accepteula -nobanner -e -h -c "<LOCATION>"'
+                'Recurse' = '& "<DEPENDENCYPATH>" /accepteula -nobanner -e -h -c -s "<LOCATION>"'
+            }
+            Path = @{
+                '32-bit' = Join-Path -Path (Get-PRPath -Bin) -ChildPath 'sigcheck.exe'
+                '64-bit' = Join-Path -Path (Get-PRPath -Bin) -ChildPath 'sigcheck64.exe'
+            }
+            TestPath = @((Join-Path -Path $RemoteStageDirectory -ChildPath 'sigcheck*.exe'))
+        }
+    }
+
+    # Begin dependency deploy logic
+    # Verify the each $Dependency exe exists
+    $Dependency | Select-Object -ExpandProperty 'Keys' -PipelineVariable 'Dep' | Foreach-Object { $Dependency.$Dep.Path.GetEnumerator() | Where-Object { !(Test-Path -Path $PSItem.Value -PathType 'Leaf') } | Foreach-Object { throw ('{0} version of {1} not detected in Bin. Place {0} executable in Bin directory and try again.' -f $PSItem.Key,$Dep) } }
+
+    foreach ($Key in $Dependency.Keys) {
+        # Track all session we are deploying this dependency to
+        $Dependency.$Key.Deploy = Invoke-Command -Session $Session -ScriptBlock { $Key = $using:Key; $Dependency = $using:Dependency; (Get-Item -Force -Path $Dependency.$Key.TestPath -ErrorAction 'SilentlyContinue' | Select-Object -First 1) -eq $null } | Where-Object { $PSItem } | Foreach-Object { Get-PSSession -InstanceId $PSItem.RunspaceId }
+        
+        foreach ($Instance in $Dependency.$Key.Deploy) {
+            try {
+                # Determine system $Architecture and select proper executable
+                $Architecture = Invoke-Command -Session $Instance -ScriptBlock { if (!(Test-Path -Path $using:RemoteStageDirectory -PathType 'Container')) { $null = New-Item -Path $using:RemoteStageDirectory -ItemType 'Directory' }; Get-WmiObject -Class 'Win32_OperatingSystem' -Property 'OSArchitecture' -ErrorAction 'Stop' | Select-Object -ExpandProperty 'OSArchitecture' }
+            } catch {
+                # Unable to get $Architecture information
+                $Warning = 'Unable to determine system architecture for {0}. Data was not gathered.' -f $Instance.ComputerName
+            }
+
+            # Ensure we are tracking a sensible $Architecture
+            if ($Architecture -and $Dependency.$Key.Path.Keys -NotContains $Architecture) {
+                $Warning = 'Unknown system architecture ({0}) detected for {1}. Data was not gathered.)' -f $Architecture, $Instance.ComputerName
+            }
+
+            # If we ran into problems with the above checks
+            if ($Warning) {
+                # Write the warning
+                Write-PRWarning -Message $Warning
+
+                # Remove the failed $Session for master and deploy list
+                $Session = $Session | Where-Object { $PSItem.ComputerName -ne $Instance.ComputerName }
+                $Dependency.$Key.Deploy = $Dependency.$Key.Deploy | Where-Object { $PSItem.ComputerName -ne $Instance.ComputerName }
+
+                # Continue to next item
+                continue
+            }
+
+            # Compute the $RemoteDependency path
+            $RemoteDependency = Join-Path -Path $RemoteStageDirectory -ChildPath (Split-Path -Leaf -Path $Dependency.$Key.Path.$Architecture)
+
+            try {
+                # Copy dependency executable to the remote machine
+                Copy-Item -Path $Dependency.$Key.Path.$Architecture -Destination $RemoteDependency -ToSession $Instance -Force -ErrorAction 'Stop'
+            } catch {
+                # Failed to copy dependency
+                throw ('Could not copy {0} to remote machine. Quitting.' -f $Key)
+            }
+        }
+    }
+    # End dependency deploy logic
+
+    # Start plguin logic
+
+    # Loop through dependencies and run the associated commands in order
+    Invoke-Command -Session $Session -ScriptBlock {
+        # Pull remote Dependency into a local variable in each session
+        $Dependency = $using:Dependency
+
+        # Get all non-exe paths in the remote stage directory
+        $Path = Get-ChildItem -Force -Path $using:RemoteStageDirectory -Exclude '*.exe' | Select-Object -ExpandProperty 'FullName'
+
+        foreach ($Key in $Dependency.Keys) {
+            # Get actual dependency path
+            $DependencyPath = Get-Item -Path $Dependency.$Key.TestPath -ErrorAction 'SilentlyContinue' | Select-Object -First 1 -ExpandProperty 'FullName'
+
+            if ($Using:Recurse){
+                # Create dependency command
+                $Command = $Dependency.$Key.Command.Recurse -Replace '<DEPENDENCYPATH>',$DependencyPath -Replace '<LOCATION>',$Using:Location
+            }
+
+            if(!$Using:Recurse){
+                # Create dependency command
+                $Command = $Dependency.$Key.Command.NoRecurse -Replace '<DEPENDENCYPATH>',$DependencyPath -Replace '<LOCATION>',$Using:Location
+            }
+
+            # Execute dependency command
+            Invoke-Expression -Command $Command | ConvertFrom-CSV
+        }
+    }
+
+    # End plugin logic
+
+    # Remove $Dependency if deployed by this plugin
+    $Dependency.Keys | Where-Object { $Dependency.$PSItem.Deploy } | Foreach-Object { Invoke-Command -Session $Dependency.$Key.Deploy -ScriptBlock { $Key = $using:Key; $Dependency = $using:Dependency; Remove-Item -Force -Path $Dependency.$Key.TestPath -ErrorAction 'SilentlyContinue' } }
+
+    # Remove created files on remote machine as cleanup
+    Invoke-Command -Session $Session -ScriptBlock {
+        # By default remove entire remote stage directory
+        $RemovePath = $using:RemoteStageDirectory
+
+        # Unless we have deployed exes there
+        Get-ChildItem -Force -Path $using:RemoteStageDirectory -Include '*.exe' | Select-Object -First 1 | Foreach-Object { $RemovePath = $Path }
+
+        # Remove the archive
+        Remove-Item -Force -Recurse -Path $RemovePath
     }
 }
