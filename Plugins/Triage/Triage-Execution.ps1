5564193047cc4b49b830ee7701a03d02988fdf6c
valrkey <10157559+valrkey@users.noreply.github.com>
2019-10-11 11:27:02 -0500

    Refactored Triage Plugins

--- Plugins/Triage/Triage-Execution.ps1
+++ Plugins/Triage/Triage-Execution.ps1
@@ -29,9 +29,9 @@
     Date Created: 5/10/2019
     Twitter: @5ynax
     
-    Last Modified By:
-    Last Modified Date:
-    Twitter:
+    Last Modified By: Gavin Prentice   
+    Last Modified Date: 10/11/2019
+    Twitter: @Valrkey
   
 #>
 
@@ -43,72 +43,6 @@ param (
     )
 
 process {
-    # Configuration Section
-    $DependencyName = @('7za','Velociraptor')
-    $StagingDirectory = 'C:\ProgramData'
-
-    # TestDependency is a helper function to be sent to the remote systems and check for existing binary dependencies
-    function TestDependency {
-        process {
-            # Get system $Architecture
-            $Architecture = Get-WmiObject 'Win32_OperatingSystem' -Property 'OSArchitecture' -ErrorAction 'SilentlyContinue' | Select-Object -ExpandProperty 'OSArchitecture'
-
-            foreach ($Name in $Args) {
-                [String]$Path = '{0}\{1}*.exe' -f $using:StagingDirectory,$Name
-                [PSCustomObject]@{
-                    Architecture = $Architecture
-                    IsPresent = (Test-Path -Path $Path)
-                    Path = $Path
-                    Name = $Name
-                }
-            }
-        }
-    }
-
-    # Create $Dependency list skeleton
-    $Dependency = @{}
-
-    foreach ($Name in $DependencyName) {
-        # Populate $Dependency with 32-bit and 64-bit path entries for each $Name
-        $Dependency.$Name = @{
-            '32-bit' = '{0}\{1}_x86.exe' -f (Get-PRPath -Bin),$Name
-            '64-bit' = '{0}\{1}_x64.exe' -f (Get-PRPath -Bin),$Name
-        }
-
-        # Verify that local binaries exist
-        $Dependency.$Name.GetEnumerator() | Where-Object { !(Test-Path -Path $PSItem.Value) } | Foreach-Object { throw ('{0} version of {1} not detected in Bin. Place {0} executable in Bin directory and try again.' -f $PSItem.Key,$Name) }
-    }
-
-    # Track which machines we deployed binaries to
-    $Deployed = @{}
-
-    # Get the $Dependency list from $Session
-    $MissingRemoteDependency = Invoke-Command -Session $Session -ScriptBlock $function:TestDependency -ArgumentList @($Dependency.Keys) | Where-Object { !$PSItem.IsPresent } | Group-Object -Property 'PSComputerName'
-
-    foreach ($RemoteDependencyGroup in $MissingRemoteDependency) {
-        # Get the $SessionInstance
-        $SessionInstance = $Session | Where-Object { $RemoteDependencyGroup.Name -eq $PSItem.ComputerName }
-
-        foreach ($RemoteDependency in $RemoteDependencyGroup.Group) {
-            # Get $DependencyPath from $RemoteDependency information
-            $DependencyPath = $Dependency.($RemoteDependency.Name).($RemoteDependency.Architecture)
-
-            # Get $Destination from $StagingDirectory
-            $Destination = '{0}\{1}' -f $StagingDirectory,(Split-Path -Leaf -Path $DependencyPath)
-
-            try {
-                # Copy the requested $RemoteDependency to $SessionInstance
-                Copy-Item -Force -ToSession $SessionInstance -Path $DependencyPath -Destination $Destination
-
-                # On success, track our action in $Deployed
-                [String[]]$Deployed.($RemoteDependency.PSComputerName) += $Destination
-            } catch {
-                throw ('Could not copy {0} to remote machine {1}: {2}. Quitting...' -f $DependencyPath,$SessionInstance.ComputerName,$PSItem)
-            }
-        }
-    }
-        
-
     # Plugin Execution
     Invoke-PRPlugin -Name Collect-PrefetchListing.ps1 -Session $Session
     Invoke-PRPlugin -Name Collect-ProcessDLLs.ps1 -Session $Session
@@ -116,21 +50,4 @@ process {
     Invoke-PRPlugin -Name Collect-RecentItemsListing.ps1 -Session $Session
     Invoke-PRPlugin -Name Collect-UserAssist.ps1 -Session $Session
     Invoke-PRPlugin -Name Retrieve-Handles.ps1 -Session $Session
-
-    foreach ($Deployment in $Deployed.GetEnumerator()) {
-        # Get the $SessionInstance
-        $SessionInstance = $Session | Where-Object { $Deployment.Key -eq $PSItem.ComputerName }
-
-        # Get $Binaries array
-        $Binaries = $Deployment.Value
-
-        try {
-            # Delete any $Deployed $Binaries
-            Invoke-Command -Session $SessionInstance -ScriptBlock { Remove-Item -Force -Recurse -Path $using:Binaries }
-        } catch {
-            # Whelp we tried, write a warning
-            Write-Warning -Message ('Failed to remove binary dependency {0} from remote system {1}' -f ($Binaries -join ','),$Deployment.Key)
-        }
-    }
-    
 }
--- Plugins/Triage/Triage-Network.ps1
+++ Plugins/Triage/Triage-Network.ps1
@@ -29,126 +29,85 @@
     Date Created: 5/10/2019
     Twitter: @5ynax
     
-    Last Modified By:
-    Last Modified Date:
-    Twitter:
+    Last Modified By: Gavin Prentice
+    Last Modified Date: 10/11/2019
+    Twitter: @Valrkey
   
 #>
 
 param (
 
     [Parameter(Mandatory=$true,Position=0)]
-    [System.Management.Automation.Runspaces.PSSession]$Session
+    [System.Management.Automation.Runspaces.PSSession[]]$Session
 
-    )
+)
 
-process{
+process {
+    # Get stage directory
+    $RemoteStageDirectory = Get-PRConfig -Property 'RemoteStagePath'
 
-    #7zip checks
-    $7zTestPath = "C:\ProgramData\7za*.exe"
-    $7zFlag = Invoke-Command -Session $Session -ScriptBlock {Test-Path $($args[0])} -ArgumentList $7zTestPath
-
-    #7zip BIN locations
-    $7za32 = ("{0}\7za_x86.exe" -f (Get-PRPath -Bin))
-    $7za64 = ("{0}\7za_x64.exe" -f (Get-PRPath -Bin))
-
-    #Velociraptor checks
-    $VeloTestPath = "C:\ProgramData\Velociraptor*.exe"
-    $VeloFlag = Invoke-Command -Session $Session -ScriptBlock {Test-Path $($args[0])} -ArgumentList $VeloTestPath
-
-    #Velociraptor BIN locations
-    $Velo_64 = ("{0}\Velociraptor_x64.exe" -f (Get-PRPath -Bin))
-    $Velo_32 = ("{0}\Velociraptor_x86.exe" -f (Get-PRPath -Bin))
-
-    if (!$7zFlag){
-
-        # Verify that 7za executables are located in (Get-PRPath -Bin)
-
-        $7z64bitTestPath = Get-Item -Path $7za64 -ErrorAction SilentlyContinue
-        $7z32bitTestPath = Get-Item -Path $7za32 -ErrorAction SilentlyContinue
-
-        if (!$7z64bitTestPath) {
-
-            Throw "64 bit version of 7za.exe not detected in Bin. Place 64bit executable in Bin directory and try again."
-
-        } elseif (!$7z32bitTestPath) {
-
-            Throw "32 bit version of 7za.exe not detected in Bin. Place 32bit executable in Bin directory and try again."
+    # Define $Dependency tracking structure
+    $Dependency = [Ordered]@{
+        SevenZip = @{
+            Command = '& "<DEPENDENCYPATH>" a -p{0} -tzip {1} <PATH>' -f $EncryptPassword,$Archive
+            Path = @{
+                '32-bit' = Join-Path -Path (Get-PRPath -Bin) -ChildPath '7za_x86.exe'
+                '64-bit' = Join-Path -Path (Get-PRPath -Bin) -ChildPath '7za_x64.exe'
+            }
+            TestPath = @('C:\Program Files\7-Zip\7z.exe',(Join-Path -Path $RemoteStageDirectory -ChildPath '7za*.exe'))
         }
     }
 
-    if (!$VeloFlag){
+    # Begin dependency deploy logic
+    # Verify the each $Dependency exe exists
+    $Dependency | Select-Object -ExpandProperty 'Keys' -PipelineVariable 'Dep' | Foreach-Object { $Dependency.$Dep.Path.GetEnumerator() | Where-Object { !(Test-Path -Path $PSItem.Value -PathType 'Leaf') } | Foreach-Object { throw ('{0} version of {1} not detected in Bin. Place {0} executable in Bin directory and try again.' -f $PSItem.Key,$Dep) } }
 
-        #Verify that Velociraptor executables are located in (Get-PRPath -Bin) (For locked files)
+    foreach ($Key in $Dependency.Keys) {
+        # Track all session we are deploying this dependency to
+        $Dependency.$Key.Deploy = Invoke-Command -Session $Session -ScriptBlock { $Key = $using:Key; $Dependency = $using:Dependency; (Get-Item -Force -Path $Dependency.$Key.TestPath -ErrorAction 'SilentlyContinue' | Select-Object -First 1) -eq $null } | Where-Object { $PSItem } | Foreach-Object { Get-PSSession -InstanceId $PSItem.RunspaceId }
 
-        $Velo_64TestPath = Get-Item -Path $Velo_64 -ErrorAction SilentlyContinue
-        $Velo_32TestPath = Get-Item -Path $Velo_32 -ErrorAction SilentlyContinue
+        foreach ($Instance in $Dependency.$Key.Deploy) {
+            try {
+                # Determine system $Architecture and select proper executable
+                $Architecture = Invoke-Command -Session $Instance -ScriptBlock { if (!(Test-Path -Path $using:RemoteStageDirectory -PathType 'Container')) { $null = New-Item -Path $using:RemoteStageDirectory -ItemType 'Directory' }; Get-WmiObject -Class 'Win32_OperatingSystem' -Property 'OSArchitecture' -ErrorAction 'Stop' | Select-Object -ExpandProperty 'OSArchitecture' }
+            } catch {
+                # Unable to get $Architecture information
+                $Warning = 'Unable to determine system architecture for {0}. Data was not gathered.' -f $Instance.ComputerName
+            }
 
-        if (!$Velo_64TestPath) {
+            # Ensure we are tracking a sensible $Architecture
+            if ($Architecture -and $Dependency.$Key.Path.Keys -NotContains $Architecture) {
+                $Warning = 'Unknown system architecture ({0}) detected for {1}. Data was not gathered.)' -f $Architecture, $Instance.ComputerName
+            }
 
-            Throw "64 bit version of Velociraptor not detected in Bin. Place 64bit executable in Bin directory and try again."
+            # If we ran into problems with the above checks
+            if ($Warning) {
+                # Write the warning
+                Write-PRWarning -Message $Warning
 
-        } elseif (!$Velo_32TestPath) {
+                # Remove the failed $Session for master and deploy list
+                $Session = $Session | Where-Object { $PSItem.ComputerName -ne $Instance.ComputerName }
+                $Dependency.$Key.Deploy = $Dependency.$Key.Deploy | Where-Object { $PSItem.ComputerName -ne $Instance.ComputerName }
 
-            Throw "32 bit version of Velociraptor not detected in Bin. Place 32bit executable in Bin directory and try again."
+                # Continue to next item
+                continue
+            }
+
+            # Compute the $RemoteDependency path
+            $RemoteDependency = Join-Path -Path $RemoteStageDirectory -ChildPath (Split-Path -Leaf -Path $Dependency.$Key.Path.$Architecture)
+
+            try {
+                # Copy dependency executable to the remote machine
+                Copy-Item -Path $Dependency.$Key.Path.$Architecture -Destination $RemoteDependency -ToSession $Instance -Force -ErrorAction 'Stop'
+            } catch {
+                # Failed to copy dependency
+                throw ('Could not copy {0} to remote machine. Quitting.' -f $Key)
+            }
         }
     }
+    # End dependency deploy logic
 
-     #Determine system architecture and select proper 7za.exe and Velociraptor executables
-    try {
-     
-        $Architecture = Invoke-Command -Session $Session -ScriptBlock {(Get-WmiObject -Class Win32_OperatingSystem -Property OSArchitecture -ErrorAction Stop).OSArchitecture}
-    
-        if ($Architecture -eq "64-bit") {
-
-            $Installexe = $7za64
-            $Velo_exe = $Velo_64
-
-        } elseif ($Architecture -eq "32-bit") {
-
-            $Installexe = $7za32
-            $Velo_exe = $Velo_32
-
-        } else {
-        
-            Write-Error ("Unknown system architecture ({0}) detected for {1}. Data was not gathered.)" -f $Architecture, $Session.ComputerName)
-            Continue
-        }
-
-    } catch {
-    
-     Write-Error ("Unable to determine system architecture for {0}. Data was not gathered." -f $Session.ComputerName)
-        Continue
-    }
-
-    # Copy 7zip and Velociraptor to remote machine
-    
-    if (!$7zFlag){
-
-        try {
-
-            Copy-Item -Path $Installexe -Destination "C:\ProgramData" -ToSession $Session -Force -ErrorAction Stop
-
-        } catch {
-
-            Throw "Could not copy 7zip to remote machine. Quitting..."
-        }  
-    }
-    
-    if (!$VeloFlag){
-
-        try {
-
-            Copy-Item -Path $Velo_exe -Destination "C:\ProgramData" -ToSession $Session -Force -ErrorAction Stop
-
-        } catch {
-
-            Throw "Could not copy Velociraptor to remote machine. Quitting..."
-        } 
-    }
-
-    #Plugin Execution
-
+    # Begin plugin logic
     Invoke-PRPlugin -Name Collect-ArpCache.ps1 -Session $Session
     Invoke-PRPlugin -Name Collect-DNSCache.ps1 -Session $Session
     Invoke-PRPlugin -Name Collect-InterfaceDetails.ps1 -Session $Session
@@ -156,18 +115,13 @@ process{
     Invoke-PRPlugin -Name Collect-NetworkProfiles.ps1 -Session $Session
     Invoke-PRPlugin -Name Collect-NetworkRoutes.ps1 -Session $Session
     Invoke-PRPlugin -Name Collect-SessionDrives.ps1 -Session $Session
+    # End plugin logic
 
-    #Delete 7zip if deployed by plugin
-    if (!$7zFlag){
-
-        $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("Remove-Item -Force -Recurse -Path C:\ProgramData\{0}") -f (Split-Path $Installexe -Leaf))
-        Invoke-Command -Session $Session -ScriptBlock $ScriptBlock | Out-Null
+    # Begin dependency cleanup logic
+    # Remove created files on remote machine as cleanup
+    Invoke-Command -Session $Session -ScriptBlock {
+        # Remove the staging directory
+        Remove-Item -Force -Recurse -Path $using:RemoteStageDirectory
     }
-    
-    #Delete Velociraptor if deployed by plugin
-    if (!$VeloFlag){
-
-        $ScriptBlock = $ExecutionContext.InvokeCommand.NewScriptBlock(("Remove-Item -Force -Recurse -Path C:\ProgramData\{0}") -f (Split-Path $Velo_exe -Leaf))
-        Invoke-Command -Session $Session -ScriptBlock $ScriptBlock | Out-Null
-    }
+    # End dependency cleanup logic
 }
