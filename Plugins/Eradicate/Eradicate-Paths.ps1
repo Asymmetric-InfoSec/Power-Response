23028757bf7ae0e30a83440978c27e6443f1848a
5ynax <5ynax@5ynax.com>
2019-10-16 10:48:52 -0500

    eradicate-paths update

--- Plugins/Eradicate/Eradicate-Paths.ps1
+++ Plugins/Eradicate/Eradicate-Paths.ps1
@@ -45,11 +45,26 @@ param (
 
     [Parameter(ParameterSetName="Path",Mandatory=$true,Position=1)]
     [Parameter(ParameterSetName="PathList",Mandatory=$true,Position=1)]
+    [String]$EradicateName,
+
+    [Parameter(ParameterSetName="Path",Mandatory=$true,Position=2)]
+    [Parameter(ParameterSetName="PathList",Mandatory=$true,Position=2)]
     [System.Management.Automation.Runspaces.PSSession[]]$Session
 
 )
 
 process{
+
+    $Output = ('{0}\{1}' -f (Get-PRPath -Output),$EradicateName)
+
+    #Get seconds for unique naming
+    $Seconds = (Get-Date -UFormat %s).Split('.')[0]
+
+    #Create output directory if needed
+    if (!(Test-Path $Output)){
+
+       $null = New-Item -Type Directory -Path $Output
+    }
 
     switch ($PSCmdlet.ParameterSetName){
 
@@ -58,22 +73,26 @@ process{
 
     }
 
-    $Scriptblock = {
-        $Error.Clear()
+    foreach ($PathItem in $Paths) {
 
-        Remove-Item -Path $Using:Paths -Recurse -Force -ErrorAction SilentlyContinue
+        $Scriptblock = {
 
-        foreach ($ErrorInstance in $Error){
-            $RemoteComputer = $env:ComputerName
-            $RemoteMessage = ('{0}: {1}' -f $RemoteComputer,$ErrorInstance) 
-            $RemoteMessage
-        }  
-    }
-            
-    [String[]]$Messages = Invoke-Command -Session $Session -Scriptblock $Scriptblock
+            try {
 
-    foreach ($Message in $Messages){
+                $null = Remove-Item -Path $Using:PathItem -Recurse -Force -ErrorAction Stop
+                $Outhash = @{ Host=$ENV:ComputerName; Path=$Using:PathItem; Eradicated=$true}
+                return [PSCustomObject]$Outhash
 
-        Write-PRWarning -Message $Message
-    }  
+            } catch {
+
+                $Outhash = @{ Host=$ENV:ComputerName; Path=$Using:PathItem; Eradicated=$false }
+                return [PSCustomObject]$Outhash
+
+            }
+        }
+
+        #Generate output from data collected 
+        $OutputPath = ('{0}\Scope_{1}_{2}.csv' -f $Output,($PathItem -Replace "\\","%5c" -Replace ":","%3a"),$Seconds)
+        Invoke-Command -Session $Session -ScriptBlock $ScriptBlock | Export-CSV -Path $OutputPath -Append -NoTypeInformation
+    }   
 }
